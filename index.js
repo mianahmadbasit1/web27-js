3f6d28c9ee9e438c89a7a39b6e2419982aa8b98c
Mian Ahmad Basit <66495447+mianahmadbasit1@users.noreply.github.com>
2022-11-10 11:48:53 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// alert("hi ahmad")
+// var x= prompt("hi ahmad whats your name");
+// document.write(x)
+// console.log("hi dear"  + ' ' + x) //concatenation of message
+
+// -----------------------------------------
+
+// var question = ("Enter your Number");
+// var age = prompt(question);
+// var newAge = (age + 5);
+// console.log("New Age = " + newAge);
+
+//------------------------------
+
+//var myName="Hi raza";
+ // myName= "raza ali";
+ //console.log("myName)
+
+//  let a=6;
+//  let b=6;
+//  c= a+b;
+//  document.write(c);
+
+
+/*   use your piece of code */ // Multi line commenting 
+//single line commenting 
+
+/*
+let subEnglishInClassNine= 25;
+let subUrduInClassNine=26;
+let totalMarksInClassNine=subEnglishInClassNine + subUrduInClassNine;
+console.log("your Number is" + subEnglishInClassNine);
+console.log("your Number is" + subUrduInClassNine);
+console.log("your Number is" + totalMarksInClassNine);
+*/
+
+//hkjsdajhjahd
+
